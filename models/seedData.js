1b7ea28b3ae703caeeac664e55d65470253082b8
Joshua Swiss <swissjoshua@gmail.com>
2015-09-26 13:32:35 +0100

    models seeded and working

---
+++ models/seedData.js
@@ -0,0 +1,35 @@
+var seeder = require('mongoose-seed');
+// var Animal = require
+
+	// Connect to MongoDB via Mongoose 
+seeder.connect('mongodb://localhost/animal-shelter', function() {
+    
+    // Load Mongoose models 
+    seeder.loadModels([
+        './models/animals.js'
+    ]);
+ 
+    // Clear specified collections 
+    seeder.clearModels(['Animal'], function() {
+ 
+        // Callback to populate DB once collections have been cleared 
+        seeder.populateModels(data);
+ 
+    });
+});
+
+var data = [
+    { 
+        'model': 'Animal',
+        'documents': [
+            {
+                'name': 'Fido',
+                'breed': 'Terrier'
+            },
+            {
+                'name': 'Gene',
+                'breed': 'Tabby'
+            }
+        ]
+    }
+];  
