e2b1222e287d7462bfd62401450961a42968495b
Joshua Swiss <swissjoshua@gmail.com>
2015-09-28 12:08:31 +0100

    showing, but form and output not linked

--- js/main.js
+++ js/main.js
@@ -2,16 +2,13 @@ console.log('blaaaaaahhhh');
 
 $(document).on('ready', function() {
 
-	// request("/animals", "get").done(appendAnimals(response));
-
-	$("#create-animal").on("submit", createAnimal);
-
+	$("#submit-animals").on("click", createAnimal);
 })
 
 function createAnimal(e){
-	e.preventDefault;
+	e.preventDefault();
 	var data = $(e.target).serialize();
-	request("/animals", "post", data).done(appendAnimals)
+	request("/animals", "post", data).done(appendAnimals);
 }
 
 function request(url, method, data) {
@@ -23,22 +20,24 @@ function request(url, method, data) {
 	})
 }
 
-function appendAnimals(animals) {
-	$.each(animals, function(index, animal) {
+function appendAnimals() {
+	$.get('/animals', function(response){
+      console.log(response);
+      var animals = response;
+      $.each(animals, function(index, animal) {
 		animalTemplate = '<h1 class="animal-name">' + animal.name + '</h1>';
 		animalTemplate += '<ul class="animal-details">';
 		animalTemplate += '<li class="breed">' + animal.breed + '</li>';
 		animalTemplate += '<li class="dob">' + animal.dob + '</li>';
 		animalTemplate += '<li class="gender">' + animal.gender + '</li>';
 		animalTemplate += '<li class="family">' + animal.family + '</li>';
-		animalTemplate += '<li class="status">' + animal.status + '</li>';
 		animalTemplate += '</ul>';
 
-   $('#show-animal').append(animalTemplate);
+   $('#show-animals').append(animalTemplate);
     })
-    .done(function() {
-      $('#create-animal').trigger('reset');
-    })
+    // .done(function() {
+    //   $('#create-animal').trigger('reset');
+    // })
 	})
 }
 
